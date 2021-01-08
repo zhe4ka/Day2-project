c3955d2a87e0563e4fbed948a83b0e6332d14489
zhe4ka <63822712+zhe4ka@users.noreply.github.com>
2021-01-08 15:07:49 -0500

    assignment 3

---
+++ assignment3/assignment3.js
@@ -0,0 +1,65 @@
+// const one=document.getElementById("container");
+// console.log(one);
+
+// const two=document.querySelector("#container");
+// console.log(two);
+
+// const three=document.getElementsByClassName("second");
+// console.log(three);
+
+// //const four=document.querySelector("#container > ol > li.third");
+
+// const four = document.getElementById('container').lastElementChild.querySelector('.third');
+// console.log(four);
+
+// //const q5 = document.getElementById('container');
+// //q5.innerText="Hello!";
+
+// const q6= document.querySelector(".footer").classList.add("main");
+
+
+//  const q7 = document.querySelector(".footer").classList.remove('main');
+
+//  let q8=document.createElement('li');
+
+//  //q9
+//  console.log(q8.innerText = "four");
+
+
+//  //q10
+//  //find the first it encounter
+//  //byId entire selection
+//  let parent = document.querySelector("ul")
+
+//  //q11
+//  console.log(parent.appendChild(q8));
+
+
+//  //q12
+//  const items = document.querySelectorAll("ol li");
+//  items.forEach(item => {
+//    item.style.backgroundColor = "green";
+//  });
+
+//  //q13
+
+ let q13=document.querySelector(".footer");
+ q13.remove();
+ /*
+1) Select the section with an id of container without using querySelector.
+2) Select the section with an id of container using querySelector.
+3) Select all of the list items with a class of "second".
+4) Select a list item with a class of third, but only the list item inside of the ol tag.
+5) Give the section with an id of container the text "Hello!".
+6) Add the class main to the div with a class of footer.
+7) Remove the class main on the div with a class of footer.
+8) Create a new li element.
+9) Give the li the text "four".
+10) Append the li to the ul element.
+12) Loop over all of the lis inside the ol tag and give them a background color of "green".
+13) Remove the div with a class of footer.
+*/
+
+//$("ul").css("font-size","5rem")
+//$("h1").addClass("big-title margin-50")
+//.hasClass("margin-50")
